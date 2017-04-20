6598cf463f87f86a422a55c43a5d6abc352a716a
Vika <vika.lapsar@gmail.com>
2017-04-20 22:33:35 +0300

    kotlin + data binding gradle updated

--- app/src/main/java/net/vnnz/apps/kotlin/tracker/ListPresenter.kt
+++ app/src/main/java/net/vnnz/apps/kotlin/tracker/ListPresenter.kt
@@ -2,7 +2,6 @@ package net.vnnz.apps.kotlin.tracker
 
 import android.databinding.BindingAdapter
 import android.widget.ListView
-import kotlinx.android.synthetic.main.activity_main.*
 import net.vnnz.apps.kotlin.tracker.adapter.ItemSelectAdapter
 import net.vnnz.apps.kotlin.tracker.pojo.ListItem
 
